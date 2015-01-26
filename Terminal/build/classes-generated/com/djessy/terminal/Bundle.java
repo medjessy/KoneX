3333fe3de765715c35ed8a2add259c209f31e6fc
Djessy <medjessy@gmail.com>
2015-01-25 19:58:10 -0600

    Terminal TopComponent window added

---
+++ Terminal/build/classes-generated/com/djessy/terminal/Bundle.java
@@ -0,0 +1,27 @@
+package com.djessy.terminal;
+/** Localizable strings for {@link com.djessy.terminal}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Terminal</i>
+     * @see TerminalTopComponent
+     */
+    static String CTL_TerminalAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_TerminalAction");
+    }
+    /**
+     * @return <i>Terminal Window</i>
+     * @see TerminalTopComponent
+     */
+    static String CTL_TerminalTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_TerminalTopComponent");
+    }
+    /**
+     * @return <i>This is a Terminal window</i>
+     * @see TerminalTopComponent
+     */
+    static String HINT_TerminalTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_TerminalTopComponent");
+    }
+    private void Bundle() {}
+}
